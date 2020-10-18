2f3853a0902d7fee3125894254f41f05fac25d3f
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-18 19:10:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,9 @@ if img:
         type1 = cat['type1'].index[0]
         class1 = cat['class'].index[0]
         
-        moves = df_[df_['type']==cat['type1'][0]]
         st.write(cat['type1'][0])
+        moves = df_[df_['type']==cat['type1'][0]]
+        
         rand = random.randrange(0, len(moves))
         move = moves.iloc[rand]['move']
         move_desc = moves.iloc[rand]['description']
