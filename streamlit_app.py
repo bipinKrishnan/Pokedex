694ffbd77d4306293718dad432b23ab799ca44e2
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-18 13:46:29 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+import torch
+from torchvision.transforms import transforms
+from efficientnet_pytorch import EfficientNet
+from PIL import Image
+import streamlit as st
+
+transform = transforms.Compose([
+    transforms.Resize((100, 100)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5),
+                          (0.5, 0.5, 0.5))
+])
+
+model = EfficientNet.from_name("efficientnet-b0")
+model._fc = nn.Linear(1280, 166)
+
+model.load_state_dict(torch.load('model.pt'))
+model.eval()
+
+img = st.file_uploader("Upload Image", type=['jpeg', 'jpg', 'png'])
+img = Image.open(img).convert('RGB')
+
+img = transform(img)
+
+out = model(img.unsqueeze(0))
+
+st.write(out.shape)
+
