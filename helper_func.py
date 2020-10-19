7cef1f7bc0809da7e207afd8d2365f887a843183
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-19 15:41:00 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -59,7 +59,7 @@ def get_audio(text):
 
 def get_gif(name):
     try:
-        u = f"https://www.google.co.in/search?q=+{''.join(name)}+&source=lnms&tbm=isch"
+        u = f"https://www.google.co.in/search?q=+{name}+&source=lnms&tbm=isch"
         #u = ''.join(f'https://tenor.com/search/{name}-pokemon-gifs')
         html = urllib.request.urlopen(u)
         soup = BeautifulSoup(html, 'html.parser')
