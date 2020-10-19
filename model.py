de5622becec6cba5addef191ef4bdc374dc19507
root <root@localhost.localdomain>
2020-10-19 21:52:16 +0530

    changed the model

--- model.py
+++ model.py
@@ -5,8 +5,8 @@ from efficientnet_pytorch import EfficientNet
 from targets import target
 
 def create_model():
-    model = EfficientNet.from_name("efficientnet-b0")
-    model._fc = nn.Linear(1280, len(target))
+    model = EfficientNet.from_name("efficientnet-b2")
+    model._fc = nn.Linear(1408, len(target))
 
     model.load_state_dict(torch.load('utils/model.pt', map_location=torch.device('cpu')))
     model.eval()
