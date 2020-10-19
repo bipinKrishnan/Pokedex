4552ec6665f00226db4b307d2eff40869fc34257
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-19 18:20:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,9 @@ if img:
     pred = make_pred(model, images[1])
 
     url_img = get_image(pred)
-    response_code = requests.get(url_img).status_code
+    #response_code = requests.get(url_img).status_code
     
-    if response_code == 200:
+    if url_img:
         st.markdown(f"![Alt Text]({url_img})")
     else:
         st.image(images[0].resize((224, 224)))
