45a78877260f280a8d2daae1390c5eaaa44fc6f6
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-19 23:39:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from efficientnet_pytorch import EfficientNet
 from targets import target
 
 def create_model():
-    model = EfficientNet.from_pretrained("efficientnet-b2")
+    model = EfficientNet.from_name("efficientnet-b2")
     model._fc = nn.Linear(1408, len(target))
 
     model.load_state_dict(torch.load('utils/model-91.0374.pt', map_location=torch.device('cpu')))
