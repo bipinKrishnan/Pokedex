0ebc08e70f06002555da821fc8764bfcf345bee8
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-20 10:54:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ df, df_ = load_df('utils/pokemon.csv', 'utils/moves.csv')
 
 model = create_model()
 
-img = st.file_uploader("Upload Image", type=['jpeg', 'jpg', 'png', 'webp'])
+img = st.file_uploader("Upload your Pokémon Image", type=['jpeg', 'jpg', 'png', 'webp'])
 
 if img:
     images = create_image(img)
@@ -32,7 +32,7 @@ if img:
         details = get_pokemon_details(pred, df, df_)
 
         text = f"-- This is {pred}, a {details[0]} type {details[1]}\n\n-- Since {pred} is a {details[0]} pokemon, it has a special move called {details[2]}\n\n-- {details[3]}\n\n_"
-        #st.text(text)
+        st.text(text)
 
         audio_bytes = get_audio(text[:-1])
         st.audio(audio_bytes, format='audio/ogg', start_time=0)
@@ -41,7 +41,7 @@ if img:
             
     else:
         text = f"This is {pred}\nCurrently, there is not much details about {pred} in my database!!!\n\n_"
-        #st.text(text)
+        st.text(text)
 
         audio_bytes = get_audio(text[:-1])
         st.audio(audio_bytes, format='audio/ogg', start_time=0)
