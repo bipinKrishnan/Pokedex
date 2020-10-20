e32887208cbd1c9fab65f3f8f76347204237e8d7
Bipin Krishanan P <bipinkrishnan1999.bk@gmail.com>
2020-10-20 11:02:37 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -46,7 +46,7 @@ def get_pokemon_details(pred, df, df_):
     return type1, class1, move, move_desc
 
 def get_audio(text):
-    tts = gTTS(text[:-3], lang='en-au')
+    tts = gTTS(text[:-3], lang='en-za')
     tts.save('hello.ogg')
     audio = open('hello.ogg', 'rb')
     audio_bytes = audio.read()
